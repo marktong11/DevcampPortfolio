27ecb2c27861d9f43de0d6fb3f7ca26abaeee55e
Mark Tong <marktong11@gmail.com>
2018-04-19 20:39:24 -0400

    added custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 topics created."
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Lorem ipsum dolor sit amet, sea an scripta discere aliquando, mazim argumentum qui et. Quo tempor legimus offendit ea. Everti nusquam facilisi ea qui, aperiam explicari argumentum ea usu, his placerat assentior ne. Ut mazim zril assueverit usu, cetero appareat definitionem in ius. Sit no homero integre vivendo, mucius everti ut sit. Essent voluptaria mea ei, sed error bonorum maiestatis id."
+    body: "Lorem ipsum dolor sit amet, sea an scripta discere aliquando, mazim argumentum qui et. Quo tempor legimus offendit ea. Everti nusquam facilisi ea qui, aperiam explicari argumentum ea usu, his placerat assentior ne. Ut mazim zril assueverit usu, cetero appareat definitionem in ius. Sit no homero integre vivendo, mucius everti ut sit. Essent voluptaria mea ei, sed error bonorum maiestatis id.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -24,15 +33,24 @@ end
 
 puts "5 skills created."
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title #{portfolio_item}",
-    subtitle: "My great service",
+    subtitle: "Rails",
     body: "Lorem ipsum dolor sit amet, sea an scripta discere aliquando, mazim argumentum qui et. Quo tempor legimus offendit ea. Everti nusquam facilisi ea qui, aperiam explicari argumentum ea usu, his placerat assentior ne. Ut mazim zril assueverit usu, cetero appareat definitionem in ius. Sit no homero integre vivendo, mucius everti ut sit. Essent voluptaria mea ei, sed error bonorum maiestatis id.",
     main_image: "http://via.placeholder.com/600x400",
     thumb_image: "http://via.placeholder.com/350x200"
   )
 end
 
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Lorem ipsum dolor sit amet, sea an scripta discere aliquando, mazim argumentum qui et. Quo tempor legimus offendit ea. Everti nusquam facilisi ea qui, aperiam explicari argumentum ea usu, his placerat assentior ne. Ut mazim zril assueverit usu, cetero appareat definitionem in ius. Sit no homero integre vivendo, mucius everti ut sit. Essent voluptaria mea ei, sed error bonorum maiestatis id.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
 
 puts "9 portfolio items created."
